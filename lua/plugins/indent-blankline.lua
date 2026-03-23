73e5f1483f1bd0c6af28f28afad5f4843f7e9eb2
Dark_Phoenix_ <92178883+0DarkPhoenix@users.noreply.github.com>
2026-03-23 23:08:37 +0100

    feat: changed tab character of indent-blankline to a long pipe character, and made RainbowBlue a lighter shade

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -22,19 +22,16 @@ return {
       vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#fca400" })
       vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#ffd700" })
       vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#45be45" })
-      vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#1042c9" })
+      vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#456bd3" })
       vim.api.nvim_set_hl(0, "RainbowIndigo", { fg = "#da70d6" })
       vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#9052bd" })
     end)
 
-    -- Use the same colors for rainbow-delimiters.nvim
-    vim.g.rainbow_delimiters = {
-      highlight = highlight,
-    }
     return {
       indent = {
         highlight = highlight,
         char = "│",
+        tab_char = "│",
       },
     }
   end,
