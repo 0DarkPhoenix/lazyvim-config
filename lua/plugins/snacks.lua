06c4baf70c756234ffeb57c2bf7b97924e23d3d5
Dark_Phoenix_ <92178883+0DarkPhoenix@users.noreply.github.com>
2026-03-30 22:46:52 +0200

    feat: Configured when to show hidden and ignored files

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,6 +3,18 @@ return {
   opts = {
     picker = {
       sources = {
+        files = {
+          hidden = true,
+          ignored = false,
+        },
+        explorer = {
+          hidden = true,
+          ignored = true,
+        },
+        grep = {
+          hidden = true,
+          ignored = false,
+        },
         notifications = {
           win = {
             preview = {
