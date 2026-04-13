f8a2dea16df09282eda9b55b6db92161d04f21fd
Dark_Phoenix_ <92178883+0DarkPhoenix@users.noreply.github.com>
2026-04-13 23:38:22 +0200

    feat: Changed accept autocomplete key from Enter to Tab

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "saghen/blink.cmp",
+    mode = "VeryLazy",
+    opts = {
+      keymap = {
+        -- 'super-tab' preset uses Tab to accept suggestions naturally
+        preset = "super-tab",
+      },
+    },
+  },
+}
