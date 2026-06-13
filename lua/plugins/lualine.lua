13e5c2e044ee75dee8ac61aba5eea7c2eb359a6c
Dark_Phoenix_ <92178883+0DarkPhoenix@users.noreply.github.com>
2026-06-13 21:24:32 +0200

    refactor: reverted default lazy setting and fixed comments

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
   opts = function(_, opts)
     opts.sections.lualine_c[3] = { "filetype", icon_only = false, separator = "", padding = { left = 1, right = 0 } }
 
-    -- Replace LazyVim's pretty_path with the full absolute path
+    -- Replaces LazyVim's pretty_path with the full absolute path
     opts.sections.lualine_c[4] = {
       "filename",
       path = 1, -- 0: Filename, 1: Relative path, 2: Absolute path, 3: Absolute path with tilde
