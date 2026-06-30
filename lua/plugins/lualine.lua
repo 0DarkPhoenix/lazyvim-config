0f868a4a05e24ba68b84c8a43b4f4d479d3ff570
Dark_Phoenix_ <92178883+0DarkPhoenix@users.noreply.github.com>
2026-06-30 19:12:27 +0200

    feat: added clock icon changing each full hour and added seconds resolution

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -10,10 +10,27 @@ return {
       path = 1, -- 0: Filename, 1: Relative path, 2: Absolute path, 3: Absolute path with tilde
     }
 
-    opts.sections.lualine_y = {
+    local hour_icons = {
+      [1] = "󱐿",
+      [2] = "󱑀",
+      [3] = "󱑁",
+      [4] = "󱑂",
+      [5] = "󱑃",
+      [6] = "󱑄",
+      [7] = "󱑅",
+      [8] = "󱑆",
+      [9] = "󱑇",
+      [10] = "󱑈",
+      [11] = "󱑉",
+      [12] = "󱑊",
+    }
+
+    opts.sections.lualine_z = {
       {
         function()
-          return vim.fn.line(".") .. "/" .. vim.fn.line("$")
+          local hour = tonumber(os.date("%I")) -- 12-hour clock, 1..12
+          local icon = hour_icons[hour] or "󰥔"
+          return icon .. " " .. os.date("%H:%M:%S") -- 24-hour clock with seconds
         end,
         padding = { left = 1, right = 1 },
       },
