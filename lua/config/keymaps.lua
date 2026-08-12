663c409af886fc06b4605ec6e44f63b75a2c8075
Dark_Phoenix_ <92178883+0DarkPhoenix@users.noreply.github.com>
2026-08-12 14:12:10 +0200

    feat: remapped F1 key to Esc

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+vim.keymap.set({ "n", "i", "v" }, "<F1>", "<Esc>", { noremap = true })
+
 -- Remap yanking keys to use their own unique registers
 vim.keymap.set({ "n", "v" }, "d", '"dd', { noremap = true })
 vim.keymap.set({ "n", "v" }, "D", '"dD', { noremap = true })
