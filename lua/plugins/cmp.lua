12122b5f195cce7d9fb1d54ab4fc9ce3f27a8efc
Dark_Phoenix_ <92178883+0DarkPhoenix@users.noreply.github.com>
2026-09-08 19:20:13 +0200

    feat: changed autocomplete order to mimic the same order as Visual Studio Code

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -9,6 +9,18 @@ return {
       --   -- still allow Enter to accept suggestions
       --   ["<CR>"] = { "accept", "fallback" },
       -- },
+      fuzzy = {
+        sorts = {
+          -- 1. Prioritize exact, case-sensitive matches first
+          "exact",
+          -- 2. Sort by fuzzy matching score
+          "score",
+          -- 3. Respect LSP server ordering (sortText)
+          "sort_text",
+          -- 4. Fallback to label name
+          "label",
+        },
+      },
       completion = {
         ghost_text = {
           enabled = false,
